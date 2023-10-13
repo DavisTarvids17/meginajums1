ed0b40c855859a31691cfaac23c77a5737ab9aa9
DavisTarvids17 <davis.tarvids1@gmail.com>
2023-10-13 16:15:11 +0300

    4.variants

---
+++ uzd2.py
@@ -0,0 +1,13 @@
+menesi=["Janvāris", "31", "Februāris", "28", "Marts", "31"]
+
+
+with open('info1.txt', 'w', encoding='utf-8') as datne:
+    for vards in menesi:
+        datne.write(vards)
+        datne.write(" ")
+
+def nolasaUnIzdruka():
+        datnee=open('info1.txt', 'r', encoding='utf-8')
+        datii=datnee.readline()
+        print(datii)
+nolasaUnIzdruka()
---
+++ uzd3.py
@@ -0,0 +1,7 @@
+# tveru 'info.txt' ar 'a', lai pieliktu (append)
+# izsaucu funkciju nolasaDrukaDatni(), kura
+# nolasa 'info1.txt' un izdrukā tās saturu
+
+with open('info1.txt', 'a', encoding='utf-8') as datne:
+        datne.write('Piektdiena ir klāt! \n')
+      
